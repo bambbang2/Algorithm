8ad042d1f762c6f4f53f9b7b79733a6e449a8845
bambbang2 <130466743+bambbang2@users.noreply.github.com>
2025-04-03 11:29:10 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,13 +4,9 @@ class Solution {
         int[] answer = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
-             if (commands[i][0] == commands[i][1]) {
-                 answer[i] = array[commands[i][0] - 1];
-             } else {
-                 int[] copy = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-                 Arrays.sort(copy);
-                 answer[i] = copy[commands[i][2] - 1];
-             }
+            int[] copy = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(copy);
+            answer[i] = copy[commands[i][2] - 1];
         }
         
         return answer;
